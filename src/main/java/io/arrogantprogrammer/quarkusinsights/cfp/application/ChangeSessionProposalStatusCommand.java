5040b5aa85564f5601c2f755fabd94c238cf74c6
Jeremy Davis <jeremyrdavis@mac.com>
2026-07-13 04:17:46 -0400

    changed formatting

--- src/main/java/io/arrogantprogrammer/quarkusinsights/cfp/application/ChangeSessionProposalStatusCommand.java
+++ src/main/java/io/arrogantprogrammer/quarkusinsights/cfp/application/ChangeSessionProposalStatusCommand.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 import java.util.UUID;
 
 public record ChangeSessionProposalStatusCommand(UUID proposalId, SessionProposalStatus newStatus) {
+
     public ChangeSessionProposalStatusCommand {
         Objects.requireNonNull(proposalId, "proposalId is required");
         Objects.requireNonNull(newStatus, "newStatus is required");
